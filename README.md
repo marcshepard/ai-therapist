68f72553abdc21af228d718b7d7417d1a0686a9e
marcshepard <marcshepard@outlook.com>
2023-11-15 15:04:17 -0800

    Got the favicon working. Added google signin button (not working yet).

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This is a super-simple prototype AI therapist built on openai. It's surprisingly effective for something so simple.
 
 Try it out at https://marcshepard.github.io/ai-therapist
+
+The app uses Google login to identify you; it will only access your email address in order to uniquely identify you.
+ 
--- default.js
+++ default.js
@@ -12,6 +12,21 @@ let INPUT_STATE = INPUT_STATES.NONE;
 let USER                // What the user likes to be called
 let OPENAI_API_KEY      // The users openai API key, used for access to (and billing for) their use of openai's API
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+}
+
 function startOrStopSession() {
     let startStopButton = document.getElementById("start_stop");
     let inputTextbox = document.getElementById("input");
